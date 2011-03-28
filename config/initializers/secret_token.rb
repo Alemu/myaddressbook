2b3e1fcc409744fceb4e19f7190d4a260a0cad14
Alemu Weldu G/Michael <kingofalex2@gmail.com>
2011-03-28 10:31:34 +0300

    First checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myaddressbook::Application.config.secret_token = '6e769e5a0c13ac4207f9b7b6cbc493db763e2da8326ab7ed7ad364f26696bb0138e7b0d40b15aef018e908adbc587232c1850b310dc2e2fb12289b6458f3c837'

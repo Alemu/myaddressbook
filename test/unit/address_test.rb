56f183fdb08ebbd4cc22e4f9667bd1b4b3e50605
Alemu Weldu G/Michael <kingofalex2@gmail.com>
2011-04-01 12:40:54 +0300

    best of all comments in my address book

--- test/unit/address_test.rb
+++ test/unit/address_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
+  test "it souldn't save if the Name is blank" do
+    
+    
     assert true
   end
 end
